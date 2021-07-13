7c49960f4ac1e08338d527e6a747b0bdd7be6308
Karan-Takh <86431118+Karan-Takh@users.noreply.github.com>
2021-07-13 10:28:50 -0400

    Keep dataframes with default numbered index to help with shifting the data.

--- AnotherDataConvert.py
+++ AnotherDataConvert.py
@@ -7,13 +7,13 @@ def comma_remover(sheet):
     # create a list of the column names and remove all the commas in the data values in the first column. --- the list may actually not be necessary.
     cols = list(sheetdf.columns)
     sheetdf[cols[0]] = sheetdf[cols[0]].str.replace(',', '')
-    sheetdf = sheetdf.set_index(cols[0])
+    # sheetdf = sheetdf.set_index(cols[0])
 
     # Create two new dataframes, one for absorbance and one for transmittance. Set the index as the first column, wavelength, for both.
     # Absorbance should always be the second column
-    abs_df = sheetdf.iloc[:, 0]
+    abs_df = sheetdf.iloc[:, 0:2]
     # Transmittance will always be the third column. 
-    trans_df = sheetdf.iloc[:, 1]
+    trans_df = sheetdf.iloc[:, 1:3]
     return abs_df, trans_df
 
 
@@ -25,14 +25,19 @@ pd.set_option("display.max_rows", None, "display.max_columns", None)
 print(comma_remover(y)[0])
 
 
+
+
 # Find NaN rows and shift data accordingly. 
 nan_rows = list()
+# Loop through each dataframe d, absorbance and transmittance
 for d in comma_remover(y):
     for index, row in d.iterrows():
         is_nan_series = row.isnull()
         if is_nan_series.any():
             nan_rows.append(index)
-    
+    # Loop through numbers in nan_rows, which are the indices of the rows with 
+    for item in nan_rows:
+        
 
 
 
