ccd6f0070ce0bb208785b51e93ca9904a7cc644b
kwiratan <kwiratan@terpmail.umd.edu>
2021-07-13 16:43:15 -0400

    Added the python versions of 2D and 3D graphs

---
+++ Graph2D.py
@@ -0,0 +1,81 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+data = pd.read_csv("C:\\Users\\kwira\\Downloads\\Day_1.csv")
+wavelength = data["wavelength"].tolist()
+
+fig, ax = plt.subplots()
+
+
+# Creates dictionary of colors in html format
+color = ['#00FFFF', '#FF61B0', '#FF6600', '#006600', '#FFCC00', '#666699', '#FF0000', '#9900CC', '#66FF33', '#009999',
+         '#FF0066', '#993300', '#FF3300', '#660033']
+
+# Function for graphing all lines in their respective colors
+colornum = 0
+minimum = []
+maximum = []
+scan = 1
+
+# for (columnName, columnData) in data.iteritems():
+#     if columnName == 'wavelength':
+#         continue
+#     else:
+#         y = data[f"{columnName}"].tolist()
+#         ax.plot(wavelength, y, label=f"{columnName}")
+#         minimum.append(min(y))
+#         maximum.append(max(y))
+
+
+for (columnName, columnData) in data.iteritems():
+    if columnName == 'wavelength':
+        continue
+    elif columnName == 'standard':
+        y = data[f"{columnName}"].tolist()
+        ax.plot(wavelength, y, label=f"{columnName}", color='black')
+        minimum.append(min(y))
+        maximum.append(max(y))
+    else:
+        if scan%5 == 0:
+            y = data[f"{columnName}"].tolist()
+            scanlist = np.full(shape=len(wavelength), fill_value=scan, dtype=int)
+            ax.plot(wavelength, y, label=f"{columnName}", color=f"{color[colornum]}")
+            minimum.append(min(y))
+            maximum.append(max(y))
+            colornum = colornum+1 # Changes the color to new color when there is a new batch
+            scan = scan+1
+        else:
+            y = data[f"{columnName}"].tolist()
+            scanlist = np.full(shape=len(wavelength), fill_value=scan, dtype=int)
+            ax.plot(wavelength, y, label=f"{columnName}", color=f"{color[colornum]}")
+            minimum.append(min(y))
+            maximum.append(max(y))
+            scan = scan + 1
+
+truemin = int(min(minimum)) - 10
+truemax = int(max(maximum)) + 10
+
+#Adds labels to graph
+plt.legend(loc='upper left')
+plt.title('Keep Light No Turns')
+plt.xlabel("Wavelength (nm)")
+plt.ylabel("Percent Transmission (%)")
+
+#Sets size of figure
+plt.ylim(truemin,truemax)
+fig = plt.gcf()
+fig.set_size_inches(9, 6)
+
+# Sets the tick mark intervals
+plt.xticks(np.arange(100, 1100+1, 100))
+
+# Creates lines to show PMT sensitive region
+xpmt1 = np.full(shape=len(list(range(truemin,truemax))), fill_value=200, dtype=int)
+ypmt1 = range(truemin, truemax)
+xpmt2 = np.full(shape=len(list(range(truemin,truemax))), fill_value=600, dtype=int)
+ypmt2 = range(truemin, truemax)
+ax.plot(xpmt1, ypmt1)
+ax.plot(xpmt2, ypmt2)
+
+plt.show()
---
+++ Graph3D.py
@@ -0,0 +1,150 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d.axes3d import Axes3D
+import numpy as np
+
+# Converts csv to dataframe
+data = pd.read_csv("C:\\Users\\kwira\\Downloads\\Day_1.csv")
+print(data)
+
+# Adds wavelength values to list for graphing
+wavelength = data["wavelength"].tolist()
+
+# Makes plot 3D
+ax = plt.axes(projection='3d')
+
+# Creates dictionary of colors in html format
+color = ['#00FFFF', '#FF61B0', '#FF6600', '#006600', '#FFCC00', '#666699', '#FF0000', '#9900CC', '#66FF33', '#009999',
+         '#FF0066', '#993300', '#FF3300', '#660033']
+
+# Function for graphing all lines in their respective colors
+colornum = 0
+scan = 1
+minimum = []
+maximum = []
+
+for (columnName, columnData) in data.iteritems():
+    if columnName == 'wavelength':
+        continue
+    elif columnName == 'standard':
+        y = data[f"{columnName}"].tolist()
+        scanlist = np.full(shape=len(wavelength), fill_value=scan, dtype=int) # Makes array full of same scan number
+        ax.plot3D(scanlist, wavelength, y, label=f"{columnName}", color='black')
+        minimum.append(min(y))
+        maximum.append(max(y))
+    else:
+        if scan%5 == 0:
+            y = data[f"{columnName}"].tolist()
+            scanlist = np.full(shape=len(wavelength), fill_value=scan, dtype=int)
+            ax.plot3D(scanlist, wavelength, y, label=f"{columnName}", color=f"{color[colornum]}")
+            minimum.append(min(y))
+            maximum.append(max(y))
+            colornum = colornum+1 # Changes the color to new color when there is a new batch
+            scan = scan+1
+        else:
+            y = data[f"{columnName}"].tolist()
+            scanlist = np.full(shape=len(wavelength), fill_value=scan, dtype=int)
+            ax.plot3D(scanlist, wavelength, y, label=f"{columnName}", color=f"{color[colornum]}")
+            minimum.append(min(y))
+            maximum.append(max(y))
+            scan = scan + 1
+
+newmin = int(min(minimum)) - 10
+newmax = int(max(maximum)) + 10
+
+# Adds labels to graph
+plt.legend(bbox_to_anchor=(1.05, 1.0), loc='upper left')
+plt.title('Day 1')
+ax.set_xlabel("Scan Number")
+ax.set_ylabel("Wavelength (nm)")
+ax.set_zlabel("Percent Transmission (%)")
+
+# Sets size of figure
+ax.set_xlim(0,scan)
+fig = plt.gcf()
+fig.set_size_inches(7, 7)
+
+# Sets the tick mark intervals
+plt.autoscale(False)
+plt.yticks(np.arange(100, 1100+1, 100))
+ax.set_zticks(np.arange(newmin, newmax+1, 10))
+
+# Makes z axis longer than x and y axes for better visibility
+ax.zaxis.set_tick_params(labelsize=1)
+for t in ax.zaxis.get_major_ticks(): t.label.set_fontsize(10)
+
+x_scale=4
+y_scale=4.5
+z_scale=4
+
+scale=np.diag([x_scale, y_scale, z_scale, 1.0])
+scale=scale*(1.0/scale.max())
+scale[3,3]=1.0
+
+def short_proj():
+  return np.dot(Axes3D.get_proj(ax), scale)
+
+ax.get_proj=short_proj
+
+# Creates rectangular prism to show the PMT sensitive region
+zpmt = list(range(newmin, newmax))
+xpmt = [0] * len(range(newmin, newmax))
+ypmt = np.full(shape=len(list(range(newmin,newmax))), fill_value=200, dtype=int)
+ax.plot3D(xpmt, ypmt, zpmt, color='b', linewidth=3)
+
+zpmt = list(range(newmin, newmax))
+xpmt = [scan] * len(range(newmin, newmax))
+ypmt = np.full(shape=len(list(range(newmin,newmax))), fill_value=200, dtype=int)
+ax.plot3D(xpmt, ypmt, zpmt, color='b', linewidth=3)
+
+zpmt = list(range(newmin, newmax))
+xpmt = [0] * len(range(newmin, newmax))
+ypmt = np.full(shape=len(list(range(newmin,newmax))), fill_value=600, dtype=int)
+ax.plot3D(xpmt, ypmt, zpmt, color='b', linewidth=3)
+
+zpmt = list(range(newmin, newmax))
+xpmt = [scan] * len(range(newmin, newmax))
+ypmt = np.full(shape=len(list(range(newmin,newmax))), fill_value=600, dtype=int)
+ax.plot3D(xpmt, ypmt, zpmt, color='b', linewidth=3)
+
+zpmt = [newmin] * len(range(0, scan+1))
+xpmt = list(range(0, scan+1))
+ypmt = np.full(shape=len(list(range(0, scan+1))), fill_value=600, dtype=int)
+ax.plot3D(xpmt, ypmt, zpmt, color='b', linewidth=3)
+
+zpmt = [newmin] * len(range(0, scan+1))
+xpmt = list(range(0, scan+1))
+ypmt = np.full(shape=len(list(range(0, scan+1))), fill_value=200, dtype=int)
+ax.plot3D(xpmt, ypmt, zpmt, color='b', linewidth=3)
+
+zpmt = [newmax] * len(range(0, scan+1))
+xpmt = list(range(0, scan+1))
+ypmt = np.full(shape=len(list(range(0, scan+1))), fill_value=200, dtype=int)
+ax.plot3D(xpmt, ypmt, zpmt, color='b', linewidth=3)
+
+zpmt = [newmax] * len(range(0, scan+1))
+xpmt = list(range(0, scan+1))
+ypmt = np.full(shape=len(list(range(0, scan+1))), fill_value=600, dtype=int)
+ax.plot3D(xpmt, ypmt, zpmt, color='b', linewidth=3)
+
+zpmt = np.full(shape=len(list(range(200,600))), fill_value=newmin, dtype=int)
+xpmt = [scan] * len(range(200,600))
+ypmt = list(range(200, 600))
+ax.plot3D(xpmt, ypmt, zpmt, color='b', linewidth=3)
+
+zpmt = np.full(shape=len(list(range(200,600))), fill_value=newmax, dtype=int)
+xpmt = [scan] * len(range(200,600))
+ypmt = list(range(200, 600))
+ax.plot3D(xpmt, ypmt, zpmt, color='b', linewidth=3)
+
+zpmt = np.full(shape=len(list(range(200,600))), fill_value=newmin, dtype=int)
+xpmt = [0] * len(range(200,600))
+ypmt = list(range(200, 600))
+ax.plot3D(xpmt, ypmt, zpmt, color='b', linewidth=3)
+
+zpmt = np.full(shape=len(list(range(200,600))), fill_value=newmax, dtype=int)
+xpmt = [0] * len(range(200,600))
+ypmt = list(range(200, 600))
+ax.plot3D(xpmt, ypmt, zpmt, color='b', linewidth=3)
+
+plt.show()
