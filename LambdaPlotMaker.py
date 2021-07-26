d9d85efb83424e5867c48b3121f80180644ba742
Karan-Takh <86431118+Karan-Takh@users.noreply.github.com>
2021-07-26 13:09:15 -0400

    Changes to LamdaPlotMaker to get it to work for S8

--- LambdaPlotMaker.py
+++ LambdaPlotMaker.py
@@ -34,7 +34,7 @@ lis = lis[10:]
 # lis.pop(4)
 
 #Creates the proper labels for the plot
-numbers = np.arange(0,62,1)
+numbers = np.arange(1,63,1)
 columns = ["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15"]
 columnids = [1, 6, 10, 14, 18, 22, 26, 31, 35, 39, 43, 47, 51, 55, 59]
 # columnids = [10,14,18,22,26,30,34,38,42,46,50,54,58,62,66]
@@ -46,13 +46,14 @@ fig,ax = plt.subplots(1)
 ax.plot(numbers,lis)
 ax.set_xticks(columnids)
 ax.set_xticklabels(columns)
-ax.set_xticklabels(columns,rotation=45)
-ax.set_xlabel("Irradiation #", fontsize = 12)
-ax.set_ylabel("Transmission (%)", fontsize = 12)
-ax.set_title("Transmission vs. Irradiation at \u03BB", fontsize = 12)
-ax.text(60,80,"\u03BB = {l} (nm)".format(l =l), fontsize = 12)
+ax.set_xticklabels(columns)
+ax.set_xlabel("Irradiation #", fontsize = 20)
+ax.set_ylabel("Transmission (%)", fontsize = 20)
+ax.set_title("Transmission vs. Irradiation at \u03BB", fontsize = 20)
+# The location of the text will probably need to be changed for different wavelengths
+ax.text(12,30,"\u03BB = {l} (nm)".format(l =l), fontsize = 20)
 
-plt.grid()
+plt.grid(linestyle = '--')
 plt.show()
 
 # C:\\Users\kdee2\Documents\GitHub\Quartz-Irradiations-Code\\S8 Transmittance.csv
