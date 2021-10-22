9ce0d8a473fb2385d75d8287facdd59a8b21c05f
Karan-Takh <86431118+Karan-Takh@users.noreply.github.com>
2021-10-22 14:00:00 -0400

    Update wavelength plotting attempt Karan.py

--- wavelength plotting attempt Karan.py
+++ wavelength plotting attempt Karan.py
@@ -2,6 +2,7 @@
 
 Koeth Group'''
 
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
@@ -9,20 +10,20 @@ import matplotlib.pyplot as plt
 x = input("Input csv file name (omit .csv): ")
 df = pd.read_csv(x + ".csv")
 
-
+print(df.head())
 w = int(input("Input wavelength: "))
 
 
 transmittances = df.iloc[w-190].tolist()
 transmittances = transmittances[2:]
 
-print(transmittances)
+# print(transmittances)
 
 
 columns = df.columns.tolist()
 columns = columns[2:]
 
-print(columns)
+
 
 
 # Make the array of the pulses (this will vary by experiment. For sample 9, it was 20,000 pulses each time)
@@ -33,15 +34,16 @@ pulses = [20000, 40000, 60000, 80000, 100000, 120000, 140000,
 
 # Make the figure
 fig, ax = plt.subplots()
-ax.set_xlabel('Pulses / Irradiation')
+ax.set_xlabel('Irradiation Number and Total Pulses')
+plt.xticks(rotation=70)
 ax.set_ylabel('Transmittance')
 ax.plot(columns, transmittances, 'o', color='g')
 
 # Add second axis
-axes1 = plt.gca()
-axes2 = axes1.twiny()
-axes2.set_xticks([pulses]) # not sure if need brackets
-axes2.set_xlabel("Pulses")
+# axes1 = plt.gca()
+# axes2 = axes1.twiny()
+# axes2.set_xticks([pulses]) # not sure if need brackets
+# axes2.set_xlabel("Pulses")
 
 
 plt.show()
